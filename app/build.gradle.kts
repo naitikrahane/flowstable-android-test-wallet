a23d19432e6b67c6960abec863194888ff406bdc
naitikrahane <naitikrahane85@gmail.com>
2026-01-02 23:25:55 +0530

    Switch to stable plugin versions (Hilt 2.48, Kotlin 1.9.10) to fix CI build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,8 +82,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.5")
 
     // Hilt - Dependency Injection
-    implementation("com.google.dagger:hilt-android:2.50")
-    kapt("com.google.dagger:hilt-android-compiler:2.50")
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
     implementation("androidx.hilt:hilt-work:1.1.0")
     kapt("androidx.hilt:hilt-compiler:1.1.0")
