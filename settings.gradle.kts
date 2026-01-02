684a0a2773fbbcf62d274fe66ed909c138cb94f9
naitikrahane <naitikrahane85@gmail.com>
2026-01-02 22:31:38 +0530

    Fix Gradle plugin resolution by adding explicit artifact mappings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,15 @@ pluginManagement {
             if (requested.id.id == "com.google.dagger.hilt.android") {
                 useModule("com.google.dagger:hilt-android-gradle-plugin:${requested.version}")
             }
+            if (requested.id.id == "com.android.application") {
+                useModule("com.android.tools.build:gradle:${requested.version}")
+            }
+            if (requested.id.id == "org.jetbrains.kotlin.android") {
+                useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
+            }
+            if (requested.id.id == "com.google.devtools.ksp") {
+                useModule("com.google.devtools.ksp:symbol-processing-gradle-plugin:${requested.version}")
+            }
         }
     }
 }
