2f03f20bce26e2e1707f31ddf5eeb22da47bdb28
Naitik Rahane <162469602+naitikrahane@users.noreply.github.com>
2026-01-02 22:16:39 +0530

    Update NetworkRepository.kt

--- app/src/main/java/com/antigravity/cryptowallet/data/blockchain/NetworkRepository.kt
+++ app/src/main/java/com/antigravity/cryptowallet/data/blockchain/NetworkRepository.kt
@@ -20,12 +20,12 @@ class NetworkRepository @Inject constructor() {
     private var _activeNetworkId = "eth"
     
     val networks = listOf(
-        Network("eth", "Ethereum", "https://mainnet.infura.io/v3/2e73eb0da821430d818d929e16963fc3", 1, "ETH", "ethereum", "https://api.etherscan.io/api"),
-        Network("bsc", "BNB Chain", "https://bsc-mainnet.infura.io/v3/2e73eb0da821430d818d929e16963fc3", 56, "BNB", "binancecoin", "https://api.bscscan.com/api"),
-        Network("matic", "Polygon", "https://polygon-mainnet.infura.io/v3/2e73eb0da821430d818d929e16963fc3", 137, "POL", "matic-network", "https://api.polygonscan.com/api"),
-        Network("base", "Base", "https://base-mainnet.infura.io/v3/2e73eb0da821430d818d929e16963fc3", 8453, "ETH", "ethereum", "https://api.basescan.org/api"),
-        Network("arb", "Arbitrum One", "https://arbitrum-mainnet.infura.io/v3/2e73eb0da821430d818d929e16963fc3", 42161, "ETH", "ethereum", "https://api.arbiscan.io/api"),
-        Network("op", "Optimism", "https://optimism-mainnet.infura.io/v3/2e73eb0da821430d818d929e16963fc3", 10, "ETH", "ethereum", "https://api-optimistic.etherscan.io/api")
+        Network("eth", "Ethereum", "https://mainnet.infura.io/v3/2e73eb0da821430d818d929e16963fc3", "https://mainnet.infura.io/v3/2e73eb0da821430d818d929e16963fc3", 1, "ETH", "ethereum", "https://api.etherscan.io/api"),
+        Network("bsc", "BNB Chain", "https://bsc-mainnet.infura.io/v3/2e73eb0da821430d818d929e16963fc3", "https://bsc-mainnet.infura.io/v3/2e73eb0da821430d818d929e16963fc3", 56, "BNB", "binancecoin", "https://api.bscscan.com/api"),
+        Network("matic", "Polygon", "https://polygon-mainnet.infura.io/v3/2e73eb0da821430d818d929e16963fc3", "https://polygon-mainnet.infura.io/v3/2e73eb0da821430d818d929e16963fc3", 137, "POL", "matic-network", "https://api.polygonscan.com/api"),
+        Network("base", "Base", "https://base-mainnet.infura.io/v3/2e73eb0da821430d818d929e16963fc3", "https://base-mainnet.infura.io/v3/2e73eb0da821430d818d929e16963fc3", 8453, "ETH", "ethereum", "https://api.basescan.org/api"),
+        Network("arb", "Arbitrum One", "https://arbitrum-mainnet.infura.io/v3/2e73eb0da821430d818d929e16963fc3", "https://arbitrum-mainnet.infura.io/v3/2e73eb0da821430d818d929e16963fc3", 42161, "ETH", "ethereum", "https://api.arbiscan.io/api"),
+        Network("op", "Optimism", "https://optimism-mainnet.infura.io/v3/2e73eb0da821430d818d929e16963fc3", "https://optimism-mainnet.infura.io/v3/2e73eb0da821430d818d929e16963fc3", 10, "ETH", "ethereum", "https://api-optimistic.etherscan.io/api")
     )
     
     val activeNetwork: Network
